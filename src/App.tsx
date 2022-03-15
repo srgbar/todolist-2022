558c5b02338e31b7a2986ba8487d0edc5c1ff68f
srgbar <srg.bar@yandex.ru>
2022-03-16 01:09:06 +0300

    lesson 1 (props)

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,24 @@
 import React from 'react';
 import './App.css';
+import {Todolist} from "./Todolist";
 
 function App() {
+
+    const tasks1 = [
+        {id: 1, title: "HTML&CSS", isDone: true},
+        {id: 2, title: "JS", isDone: true},
+        {id: 3, title: "ReactJs", isDone: false}
+    ]
+    const tasks2 = [
+        {id: 1, title: "Hello world", isDone: true},
+        {id: 2, title: "I am Happy ", isDone: false},
+        {id: 3, title: "Yo", isDone: false}
+    ]
+
     return (
         <div className="App">
-            <Todolist/>
+            <Todolist title={"What to learn"} tasks={tasks1}/>
+            <Todolist title={"Songs"} tasks={tasks2}/>
         </div>
     );
 }
@@ -12,22 +26,3 @@ function App() {
 export default App;
 
 
-const Todolist = () => {
-    return <div>
-        <h3>What to learn</h3>
-        <div>
-            <input/>
-            <button>+</button>
-        </div>
-        <ul>
-            <li><input type="checkbox" checked={true}/> <span>HTML&CSS</span></li>
-            <li><input type="checkbox" checked={true}/> <span>JS</span></li>
-            <li><input type="checkbox" checked={false}/> <span>React</span></li>
-        </ul>
-        <div>
-            <button>All</button>
-            <button>Active</button>
-            <button>Completed</button>
-        </div>
-    </div>
-};
