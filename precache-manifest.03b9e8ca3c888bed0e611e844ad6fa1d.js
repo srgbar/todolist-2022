2d3ce46b5c76d7ac476f0e24db0111e151f7ee8b
srgbar <srg.bar@yandex.ru>
2022-03-16 02:27:29 +0300

    Updates

---
+++ precache-manifest.03b9e8ca3c888bed0e611e844ad6fa1d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9bd3174bc6b5264f9ae0d9df1a973a2f",
+    "url": "/todolist-2022/index.html"
+  },
+  {
+    "revision": "a490ced419d9fa3279cc",
+    "url": "/todolist-2022/static/css/main.6f30ddfc.chunk.css"
+  },
+  {
+    "revision": "76f8d08f09736743dc24",
+    "url": "/todolist-2022/static/js/2.450f50a3.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/todolist-2022/static/js/2.450f50a3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a490ced419d9fa3279cc",
+    "url": "/todolist-2022/static/js/main.352f3ab7.chunk.js"
+  },
+  {
+    "revision": "8453bca201d088c6bca5",
+    "url": "/todolist-2022/static/js/runtime-main.cb262792.js"
+  }
+]);
