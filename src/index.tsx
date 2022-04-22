97bf32af850eb52dcd343688823d854e69006bc6
srgbar <srg.bar@yandex.ru>
2022-04-22 22:32:21 +0300

    migrated HashRouter in index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -5,12 +5,14 @@ import * as serviceWorker from './serviceWorker';
 import {Provider} from "react-redux";
 import {store} from "./app/store";
 import App from "./app/App";
+import {HashRouter} from "react-router-dom";
 
 ReactDOM.render(
-    <Provider store={store}>
-        <App/>
-    </Provider>
-    , document.getElementById('root'));
+    <HashRouter>
+        <Provider store={store}>
+            <App/>
+        </Provider>
+    </HashRouter>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
